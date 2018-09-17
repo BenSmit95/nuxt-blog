7c19781137606c5f2fa2e46e4d012d6c24218460
Benjamin Smit <benjismit@live.nl>
2018-09-17 20:07:29 +0200

    setup basic pages

---
+++ pages/about/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about-page">
+    <h1>This is my absolutely fantastic "About Me" Page!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia sit repellat dolor velit voluptates?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia sit repellat dolor velit voluptates?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia sit repellat dolor velit voluptates?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia sit repellat dolor velit voluptates?</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.about-page {
+  padding: 30px;
+  text-align: center;
+}
+</style>
---
+++ pages/posts/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="posts-page">
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.posts-page {
+  align-items: center;
+  display: flex;
+  justify-content: center;
+}
+</style>
